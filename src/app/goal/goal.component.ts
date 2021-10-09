709ef39f98c84fa547ef3ead6b1a1d14c4d7e51d
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-10-09 13:36:27 +0300

    add boostrap

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -20,6 +20,15 @@ import { Goal } from '../goal';
 toggleDetails(index){
   this.goals[index].showDescription = !this.goals[index].showDescription;
 }
+  deleteGoal(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+
+      if (toDelete) {
+        this.goals.splice(index, 1)
+      }
+    }
+  }
 
   completeGoal(isComplete, index) {
     if (isComplete) {
