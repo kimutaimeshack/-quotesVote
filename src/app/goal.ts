7760380f6ff538dbd44350a06b2b65c0840fc5c6
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-10-11 20:29:42 +0300

    Auto-generated commit [ci skip]

--- src/app/goal.ts
+++
@@ -1,8 +0,0 @@
-
-
-export class Goal {
-  public showDescription: boolean;
-  constructor(public id: number, public myname: string, public name: string, public description: string, public author: string, public completeDate: Date) {
-    this.showDescription = false;
-  }
-}
